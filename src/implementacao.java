6a8cb5fa7d22082b8b79c47a829b75d7adf22970
matzikaaa <matschneiderr@gmail.com>
2025-06-10 19:36:04 -0300

    Commit Implementação - arvores avl

---
+++ src/implementacao.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+    ArvoreAVL arvore = new ArvoreAVL();
+
+    int[] chaves = {10, 20, 30, 40, 50, 25};
+
+    for (int chave : chaves) {
+        arvore.raiz = arvore.inserir(arvore.raiz, chave);
+    }
+
+    System.out.println("Percurso em ordem da árvore AVL:");
+    arvore.percursoEmOrdem(arvore.raiz);
+}
